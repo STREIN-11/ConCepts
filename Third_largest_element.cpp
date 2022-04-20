063fc7071aaa3cad0b6b286f7671acffcf63b1e9
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2022-04-21 00:37:23 +0530

    Add files via upload

---
+++ Third_largest_element.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+  public:
+    int thirdLargest(int a[], int n)
+    {
+         //Your code here
+        if(n>=3)
+        {
+        sort(a,a+n);
+        int temp[n],j=0;
+        for(int i=0;i<n-1;i++)
+        {
+            if(a[i]!=a[i+1])
+               temp[j++] = a[i];
+        }
+        temp[j++] = a[n-1];
+        
+       for (int i=0; i<j; i++)
+           a[i] = temp[i];
+        
+        return a[j-3];
+        }
+        
+        else
+           return -1;
+    }
+
+};
+
+// { Driver Code Starts.
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	   Solution obj;
+	    cout<<obj.thirdLargest(a,n)<<endl;
+    }
+}     // } Driver Code Ends
