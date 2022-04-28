b274b5be5f9ca9f2529f1444c9d4f8bd10348751
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2022-04-29 01:29:54 +0530

    Add files via upload

---
+++ link_stack_prack.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+struct Node *top = NULL;
+
+void print(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Elements are : %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+int empty(struct Node *ptr)
+{
+    if (ptr == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int full(struct Node *ptr)
+{
+    struct Node *p = (struct Node *)malloc(sizeof(struct Node));
+    if (p == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+struct Node *push(struct Node *q, int val)
+{
+    if (full(q))
+    {
+        printf("Stack is Over Flow");
+    }
+    else
+    {
+        struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+        n->data = val;
+        n->next = q;
+        q = n;
+        return q;
+    }
+}
+
+int pop(struct Node *ptr)
+{
+    if (empty(ptr))
+    {
+        printf("Stack Has No Elements");
+    }
+    else
+    {
+        struct Node *h = ptr;
+        top = ptr->next;
+        int x = h->data;
+        free(h);
+    }
+}
+
+int main()
+{
+    while (1)
+    {
+        int input, n, m;
+        printf("1.Push, 2.PoP : ");
+        scanf("%d", &input);
+        if (input == 1)
+        {
+            // struct Node *top = NULL;
+            printf("How Much input : ");
+            scanf("%d", &m);
+            for (int i = 0; i <= m; i++)
+            {
+                printf("Enter Your Number : ");
+                scanf("%d", &n);
+                top = push(top, n);
+            }
+            pop(top);
+            print(top);
+        }
+        else
+            printf("No Module");
+    }
+    return 0;
+}
