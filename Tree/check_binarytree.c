10eb43882ea12f61557f4396f62c49bbe7600b86
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2022-06-30 21:17:08 +0530

    Update check_binarytree.c

--- Tree/check_binarytree.c
+++ Tree/check_binarytree.c
@@ -6,6 +6,7 @@ struct node{
     struct node* left;
     struct node* right;
 };
+
 struct node* createNode(int data){
     struct node *n; // creating a node pointer
     n = (struct node *) malloc(sizeof(struct node)); // Allocating memory in the heap
