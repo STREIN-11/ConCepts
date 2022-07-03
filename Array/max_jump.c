f3ef0747b1e5cc6691fed6fdc9a45c9f76587508
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2022-07-03 13:02:35 +0530

    Update max_jump.c

--- Array/max_jump.c
+++ Array/max_jump.c
@@ -1,16 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
-int max(){
-
-}
-
-int main(){
-    int size;
-    printf ("Enter Size : ");
-    scanf("%d",&size);
-    int arr[size];
-    
-
-    return 0;
-}
 
