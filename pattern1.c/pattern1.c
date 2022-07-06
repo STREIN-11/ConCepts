f2eea578ff890f078fb32953efcf07859eebc6d6
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2022-07-07 01:03:24 +0530

    Add files via upload

---
+++ pattern1.c/pattern1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//    *****
+//   *****
+//  *****
+// *****
+//*****
+
+int main(){
+    int row,colom;
+    printf("Enter Row : ");
+    scanf("%d",&row);
+
+    int count = colom-1;
+    for (int i = 1; i < row; i++)
+    {
+        for (int j = 1; j < row-i; j++)
+        {
+            printf(" ");   
+        }
+        for (int i = 1; i <= row; i++)
+        {
+            printf("*");
+        }
+        printf("\n");     
+    }
+    return 0;
+}
