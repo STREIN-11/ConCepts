798994f16455156847302fac35ec173c7ed28b25
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2022-07-14 20:33:51 +0530

    Add files via upload

---
+++ count_odd_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+    int n, count = 0, l = 0;
+    printf("Enter A number : ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i; j <= n; j++)
+        {
+            if (i % j == 0)
+            {
+                count++;
+            }
+        }
+        if (count % 2 == 0)
+        {
+            l++;
+        }
+    }
+    printf("R : %d", l);
+
+    return 0;
+}
