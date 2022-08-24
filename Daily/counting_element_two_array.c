5be9bed266334ad8b5e5b8dfbe42664df23c18f5
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2022-08-25 00:59:39 +0530

    Add files via upload

---
+++ Daily/counting_element_two_array.c
@@ -0,0 +1,59 @@
+/*
+m = 6, n = 6
+arr1[] = {1,2,3,4,7,9}
+arr2[] = {0,1,2,1,1,4}
+Output: 4 5 5 6 6 6
+Explanation: Number of elements less than
+or equal to 1, 2, 3, 4, 7, and 9 in the
+second array are respectively 4,5,5,6,6,6*/
+
+//{ Driver Code Starts
+// C++ implementation of For each element in 1st 
+// array count elements less than or equal to it
+// in 2nd array
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+
+// } Driver Code Ends
+// function to count for each element in 1st array,
+// elements less than or equal to it in 2nd array
+class Solution{
+  public:
+    vector<int> countEleLessThanOrEqual(int arr1[], int arr2[], 
+                                 int m, int n)
+    {
+        vector<int>ans;
+        sort(arr2,arr2+n);
+        for(int i=0;i<m;i++){
+            int x=upper_bound(arr2,arr2+n,arr1[i])-arr2;
+            ans.push_back(x);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m,n;
+        cin>>m>>n;
+        int arr1[m],arr2[n];
+        for(int i=0;i<m;i++)
+        cin>>arr1[i];
+        for(int j=0;j<n;j++)
+        cin>>arr2[j];
+        Solution obj;
+        vector <int> res = obj.countEleLessThanOrEqual(arr1, arr2, m, n);
+        for (int i = 0; i < res.size (); i++) cout << res[i] << " ";
+        
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
