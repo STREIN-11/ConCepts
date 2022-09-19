aecab2df4f949ced22d73a3a85618eba34dd91b7
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2022-09-19 12:32:26 +0530

    Add files via upload

---
+++ Daily/smallest_number_left.c
@@ -0,0 +1,50 @@
+/*
+Input: n = 3
+a = {1, 6, 2}
+Output: -1 1 1
+Explaination: There is no number at the 
+left of 1. Smaller number than 6 and 2 is 1.*/
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> leftSmaller(int n, int a[]){
+        stack<int> s;
+        vector<int> ans(n,-1);
+        for(int i=0;i<n;i++){
+            while(!s.empty() and a[s.top()]>=a[i]) s.pop();
+            if(!s.empty()) ans[i]=a[s.top()];
+            s.push(i);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i = 0;i < n;i++)
+            cin>>a[i];
+        
+        Solution ob;
+        vector<int> ans = ob.leftSmaller(n, a);
+        for(int i = 0;i < n;i++)
+            cout<<ans[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
