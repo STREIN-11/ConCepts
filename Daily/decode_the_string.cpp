19ff6bcf7417289831bbbbcdb2d446c8e24db643
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2022-10-11 14:25:45 +0530

    Add files via upload

---
+++ Daily/decode_the_string.cpp
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+     string decodedString(string s){
+
+        string res="";
+
+        stack<char> st;
+
+        st.push(s[0]);
+
+        for(int i=1;i<s.size();i++){
+
+            if(s[i]==']'){
+
+                while(!st.empty()&&st.top()!='['){
+
+                    res=st.top()+res;
+
+                    st.pop();
+
+                }
+
+                if(!st.empty())
+
+                    st.pop();
+
+                string rnum="";
+
+                while(!st.empty()&&st.top()>='0'&&st.top()<='9'){
+
+                    rnum=st.top()+rnum;
+
+                    st.pop();
+
+                }
+
+                string sr=res;
+
+                for(int j=0;j<stoi(rnum)-1;j++){
+
+                    res+=sr;
+
+                }
+
+                for(auto x:res){
+
+                    st.push(x);
+
+                }
+
+                res="";
+
+            }
+
+            else{
+
+                st.push(s[i]);
+
+            }
+
+        }
+
+        res="";
+
+        while(!st.empty()){
+
+            res=st.top()+res;
+
+            st.pop();
+
+        }
+
+        return res;
+
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        
+        Solution ob;
+        cout<<ob.decodedString(s)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
