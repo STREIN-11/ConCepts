5b742a8b1a78e81db4263f86e194be75a1512a2c
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2022-11-18 23:00:10 +0530

    Update count_odd_factor.c

--- count_odd_factor.c
+++ count_odd_factor.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
     int n, count = 0, l = 0;
     printf("Enter A number : ");
     scanf("%d", &n);
