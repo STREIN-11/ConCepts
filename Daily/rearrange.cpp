2eac3b8ec0ff12398527ae9dc2e1f7a2692a9421
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2022-12-01 01:46:30 +0530

    Add files via upload

---
+++ Daily/rearrange.cpp
@@ -0,0 +1,86 @@
+/*
+Input:
+n = 6
+arr[] = {1,2,3,4,5,6}
+Output: 6 1 5 2 4 3
+Explanation: Max element = 6, min = 1, 
+second max = 5, second min = 2, and 
+so on... Modified array is : 6 1 5 2 4 3.*/
+//{ Driver Code Starts
+// C++ program to rearrange an array in minimum 
+// maximum form 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // This function wants you to modify the given input
+    // array and no need to return anything
+    // arr: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    void rearrange(long long *arr, int n) 
+    { 
+        deque<int> dq;
+        
+    	for(int i=0; i<n; i++)
+    	    dq.push_back(arr[i]);
+    	    
+    	for(int i=0; i<n; i++)
+    	{
+    	    if(i%2 == 0)
+    	    {
+    	        arr[i] = dq.back();
+    	        dq.pop_back();
+    	    }
+    	    else
+    	    {
+    	        arr[i] = dq.front();
+    	        dq.pop_front();
+    	    }
+    	}
+    }
+
+    
+};
+
+//{ Driver Code Starts.
+
+// Driver program to test above function 
+int main() 
+{
+    int t;
+    
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        
+        //size of array
+        int n;
+        cin >> n;
+        
+        long long arr[n];
+        
+        //adding elements to the array
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        
+        //calling rearrange() function
+        ob.rearrange(arr, n);
+        
+        //printing the elements
+        for (int i = 0; i < n; i++) 
+		    cout << arr[i] << " ";
+		
+		cout << endl;
+    }
+	return 0; 
+} 
+
+// } Driver Code Ends
