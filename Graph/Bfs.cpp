95b182ffc4171e65bbae9535280173380b2141d8
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2022-12-06 03:35:32 +0530

    Add files via upload

---
+++ Graph/Bfs.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Graph{
+    int V;
+    vector<list<int>> adj;
+
+public:
+    Graph(int V);
+    void addedge(int v,int w);
+    void bfs(int s);
+};
+
+Graph::Graph(int V){
+    this->V = V;
+    adj.resize(V);
+}
+
+void Graph::addedge(int v,int w){
+    adj[v].push_back(w);
+}
+
+void Graph::bfs(int s){
+    vector<bool> visited;
+    visited.resize(V,false);
+
+    list<int> queue;
+
+    visited[s] = true;
+    queue.push_back(s);
+
+    while(!queue.empty()){
+        s = queue.front();
+        cout<<s<<" ";
+        queue.pop_front();
+
+        for (auto adjecent: adj[s]){
+            if (!visited[adjecent]){
+                visited[adjecent] = true;
+                queue.push_back(adjecent);
+            }
+        }
+    }
+}
+
+int main(){
+    Graph g(4);
+    g.addedge(0, 1);
+    g.addedge(0, 2);
+    g.addedge(1, 2);
+    g.addedge(2, 0);
+    g.addedge(2, 3);
+    g.addedge(3, 3);
+    printf("Result : ");
+    g.bfs(2);
+
+return 0;
+}
