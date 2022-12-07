65c3ec57162e04b5c9e87a08a527dfc584297e21
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2022-12-07 17:37:51 +0530

    Add files via upload

---
+++ Tree/K_th_small_element.cpp
@@ -0,0 +1,164 @@
+/*
+Given a BST and an integer K. Find the Kth Smallest element in the BST using O(1) extra space. 
+Input:
+      2
+    /   \
+   1     3
+K = 2
+Output: 2
+Explanation: 2 is the 2nd smallest element in the BST
+Input:
+        2
+      /  \
+     1    3
+K = 5
+Output: -1
+Explanation: There is no 5th smallest element in the BST as the size of BST is 3*/
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/*Complete the function below
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  private:
+        int Iterations = 0;
+        int k;
+        int ans =-1;
+    
+    public:
+        void search(Node* bst)
+        {
+            if(bst)
+            {
+                search(bst->left);
+                
+                Iterations++;
+                
+                if(Iterations == k)
+                    ans = bst->data;
+                else if(Iterations > k)
+                    return ;
+                    
+                search(bst->right);
+            }
+        }
+     int KthSmallestElement(Node *bst, int K)
+        {
+            k = K;
+            
+            search(bst);
+            
+            return ans;
+        }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        getline(cin, s);
+        int k = stoi(s);
+        //  getline(cin, s);
+        Solution obj;
+        cout << obj.KthSmallestElement(root, k) << endl;
+        // cout<<"~"<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
