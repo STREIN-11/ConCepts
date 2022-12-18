f77d1618dc12c308c89bc41c47e8f70b8d46d8c9
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2022-12-18 18:09:51 +0530

    Update check_array_BinaryHeap.cpp

--- Heap/check_array_BinaryHeap.cpp
+++ Heap/check_array_BinaryHeap.cpp
@@ -1,3 +1,31 @@
+// Input:  arr[] = {90, 15, 10, 7, 12, 2} 
+// Output: True
+// The given array represents below tree
+//        90
+//      /    \
+//    15      10
+//   /  \     /
+//  7    12  2 
+// The tree follows max-heap property as every
+// node is greater than all of its descendants.
+
+// Input:  arr[] = {9, 15, 10, 7, 12, 11} 
+// Output: False
+// The given array represents below tree
+//        9
+//      /    \
+//    15      10
+//   /  \     /
+//  7    12  11
+// The tree doesn't follows max-heap property 9 is 
+// smaller than 15 and 10, and 10 is smaller than 11.
+
+// A Simple Solution is to first check root if it’s greater than all of its descendants. Then check for children of the root. Time complexity of this solution is O(n2)
+
+// An Efficient Solution is to compare root only with its children (not all descendants), if root is greater than its children and the same is true for all nodes,
+// then tree is max-heap (This conclusion is based on transitive property of > operator, i.e., if x > y and y > z, then x > z).
+// The last internal node is present at index (n-2)/2 assuming that indexing begins with 0.
+    
 #include<bits/stdc++.h>
 using namespace std;
 bool check(int *arr,int size){
