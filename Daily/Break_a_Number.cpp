594c1a868ff47966739a7aeb0eccd851d189bb3e
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-01-08 22:55:44 +0530

    Update Break_a_Number.cpp

--- Daily/Break_a_Number.cpp
+++ Daily/Break_a_Number.cpp
@@ -2,6 +2,9 @@
 Given a really large number N, break it into 3 whole numbers such that they sum up to the original number and
  find the number of ways to do so. Since this number can be very large, return it modulo 109+7. 
 Input:
+
+
+
 N = 2
 Output:
 6
