d8328c25573404e5d9557e1a318b4d4f905d0c56
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-01-12 23:58:47 +0530

    Update Geek_and_string.cpp

--- Daily/Geek_and_string.cpp
+++ Daily/Geek_and_string.cpp
@@ -8,9 +8,7 @@ using namespace std;
 
 class Solution{
 public:
-    vector<int> prefixCount(int N, int Q, string li[], string query[])
-    {
-
+    vector<int> prefixCount(int N, int Q, string li[], string query[]){
  map<string,int>m;
 
         for(int i=0;i<N;i++){
