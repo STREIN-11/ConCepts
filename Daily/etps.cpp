0f4eef6c4e48977a3c7227fdcd3cec6e4c11c2c1
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-02-12 23:24:14 +0530

    Update etps.cpp

--- Daily/etps.cpp
+++ Daily/etps.cpp
@@ -1,5 +1,4 @@
 /*
-
 Geek's Paint Shop is one of the famous shop in Geekland, but 2014 Earthquake caused disarrangement of the items in his shop.
  Each item in his shop is a 40-digit alpha numeric code .Now Chunky wants to retain the reputation of his shop, for that he has to arrange 
 all the distinct items in lexicographical order.Your task is to arrange the all the distinct items in lexicographical ascending order 
