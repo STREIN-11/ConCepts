0770481e866ff23c59bd9e0e1a01bb05a15859d5
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-02-23 21:36:35 +0530

    Update reverse.java

--- src/reverse.java
+++ src/reverse.java
@@ -1,3 +1,8 @@
+//Input: 1->2->3->4->5->6->7->8->NULL, K = 3 
+// Output: 3->2->1->6->5->4->8->7->NULL 
+// Input: 1->2->3->4->5->6->7->8->NULL, K = 5 
+// Output: 5->4->3->2->1->8->7->6->NULL 
+
 import java.util.*;
 class Node {
     int data;
