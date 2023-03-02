c3f5d56be77fc2776da12e63f653fcb6e84a202d
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-03-03 04:59:35 +0530

    Add files via upload

---
+++ Daily/cutting_rectangles.cpp
@@ -0,0 +1,49 @@
+/*
+Given a rectangle of dimensions L x B find the minimum number (N) of identical squares of maximum side that can be cut
+ out from that rectangle so that no residue remains in the rectangle. Also find the dimension K of that square.
+
+Input: L = 2, B = 4
+Output: N = 2, K = 2
+Explaination: 2 squares of 2x2 dimension.
+
+Example 2:
+
+Input: L = 6, B = 3
+Output: N = 2, K = 3
+Explaintion: 2 squares of 3x3 dimension. */
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<long long int> minimumSquares(long long int L, long long int B)
+    {
+        long long int K=__gcd(L,B);
+        long long int N=L/K * B/K;
+        return {N,K};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long int L, B;
+        cin>>L>>B;
+        
+        Solution ob;
+        vector<long long int> ans = ob.minimumSquares(L, B);
+        cout<<ans[0]<<" "<<ans[1]<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
