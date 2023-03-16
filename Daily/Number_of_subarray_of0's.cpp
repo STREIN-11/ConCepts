2411b5dbedbc1d4043d16ca0882c9dfd55419b7f
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-03-17 01:52:20 +0530

    Add files via upload

---
+++ Daily/Number_of_subarray_of0's.cpp
@@ -0,0 +1,71 @@
+/*
+You are given an array arr  of length N of 0's and 1's. Find the total number of subarrays of 0's
+Example 1:
+
+Input:
+N = 4
+arr[] = {0, 0, 1, 0}
+Output:
+4
+Explanation:
+Following are the subarrays of
+length 1: {0}, {0}, {0} - 3
+length 2: {0, 0} - 1
+Total Subarrays: 3 + 1 = 4
+
+Example 2:
+
+Input:
+N = 4
+arr[] = {0, 0, 0, 0}
+Output:
+10
+Explanation:
+Following are the subarrays of
+length 1: {0}, {0}, {0}, {0} - 4
+length 2: {0, 0}, {0, 0}, {0, 0} - 3
+length 3: {0, 0, 0}, {0, 0, 0} - 2
+length 4: {0, 0, 0, 0} - 1
+Total Subarrays: 4 + 3 + 2 + 1 = 10*/
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+long long int no_of_subarrays(int n, vector<int> &arr) {
+long long int count = 0;
+for(int i = 0 ;i < arr.size();i++){
+int counter = 1 ;
+while(i<arr.size() && arr[i]==0){
+count = count+ counter ;
+counter++;
+i++;
+}
+}
+return count ;
+}
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        cout << no_of_subarrays(n, arr) << endl;
+    }
+}
+
+// } Driver Code Ends
