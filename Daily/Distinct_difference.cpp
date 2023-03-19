24e036f35925db7fa555a745aac1cbddf6800350
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-03-19 23:57:52 +0530

    Add files via upload

---
+++ Daily/Distinct_difference.cpp
@@ -0,0 +1,110 @@
+/*
+Given an array A[] of length N. For each index i (1<=i<=N), find the diffference between the number of distinct element 
+in it's left and right side in the array. 
+Input:
+N = 3
+arr[] = {4, 3, 3}
+Output: {-1, 0, 2}
+Explanation: For index i=1, there are 0 distinct element in the left side of it, and 1 distinct element(3) in it's right side. So difference is 0-1 = -1. 
+
+Similarly for index i=2, there is 1 distinct element (4) in left side of it, and 1 distinct element(3) in it's right side. So difference is 1-1 = 0.
+
+Similarly for index i=3, there are 2 distinct elements (4 and 3) in left side of it, and 0 distinct elements in it's left side. So difference is 2-0 = 2.
+
+Input:
+N = 4
+arr[] = {4, 4, 3, 3}
+Output: {-2, 0, 0, 2}
+Explanation: For index i=1, there are 0 distinct element in the left side of it, and 2 distinct element(4 and 3) in it's right side. So difference is 0-2 = -2.
+
+Similarly for index i=2, there is 1 distinct element (4) in left side of it, and 1 distinct element(3) in it's right side. So difference is 1-1 = 0.
+
+Similarly for index i=4, there are 2 distinct elements (4 and 3) in left side of it, and 0 distinct element in it's right side. 
+So difference is 2-0 = 2.*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> getDistinctDifference(int n, vector<int> &arr)
+    {
+        vector<int> CountLeft(n), CountRight(n);
+        unordered_set<int> s;
+
+        for (int i = 0; i < n; ++i)
+        {
+            s.insert(arr[i]);
+            CountLeft[i] = s.size();
+        }
+
+        s.clear();
+
+        for (int i = n - 1; i >= 0; --i)
+        {
+            s.insert(arr[i]);
+            CountRight[i] = s.size();
+        }
+
+        for (int i = 0; i < n - 1; ++i)
+        {
+            arr[i] = CountLeft[i - 1] - CountRight[i + 1];
+        }
+
+        arr[n - 1] = CountLeft[n - 2];
+
+        return arr;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        Array::input(A,N);
+        
+        Solution obj;
+        vector<int> res = obj.getDistinctDifference(N, A);
+        
+        Array::print(res);
+        
+    }
+}
+
+// } Driver Code Ends
