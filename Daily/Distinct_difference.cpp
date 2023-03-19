424a48419ed0f9967a654ec37b0544c37cb55327
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-03-19 23:58:11 +0530

    Update Distinct_difference.cpp

--- Daily/Distinct_difference.cpp
+++ Daily/Distinct_difference.cpp
@@ -6,9 +6,7 @@ N = 3
 arr[] = {4, 3, 3}
 Output: {-1, 0, 2}
 Explanation: For index i=1, there are 0 distinct element in the left side of it, and 1 distinct element(3) in it's right side. So difference is 0-1 = -1. 
-
 Similarly for index i=2, there is 1 distinct element (4) in left side of it, and 1 distinct element(3) in it's right side. So difference is 1-1 = 0.
-
 Similarly for index i=3, there are 2 distinct elements (4 and 3) in left side of it, and 0 distinct elements in it's left side. So difference is 2-0 = 2.
 
 Input:
