4ce5e31d049dc87687219b056f406d5d01c780cd
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-04-01 02:44:07 +0530

    Update Make_Array_Element_Equal.cpp

--- Special_Type/Make_Array_Element_Equal.cpp
+++ Special_Type/Make_Array_Element_Equal.cpp
@@ -4,7 +4,6 @@ You are given an integer N. Consider an array arr having N elements where arr[i]
 You are allowed to perform the given operation on the array any number of times:
 
 1) Select two indices i and j and increase arr[i] by 1 and decrease arr[j] by 1.
-
 Your task is to find the minimum number of such operations required to make all the elements of the array equal.
 
 Input:
