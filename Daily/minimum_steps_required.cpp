dbee21c1cbce6ad7d9da7e4f5fa485f423d493a9
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-04-04 04:11:55 +0530

    Add files via upload

---
+++ Daily/minimum_steps_required.cpp
@@ -0,0 +1,58 @@
+/*
+Given a string str consisting of only two characters 'a' and 'b'. You need to find the minimum steps required to
+ make the string empty by removing consecutive a's and b's.
+
+Input:
+str = "bbaaabb"
+Output:
+2
+Explanation:
+Operation 1: Removal of all a's modifies str to "bbbb".
+Operation 2: Removal of all remaining b's makes str
+empty.
+Therefore, the minimum number of operations required
+is 2.
+
+Input:
+str = "aababaa"
+Output:
+3
+Explanation:
+Operation 1: Removal of b's modifies str to "aaabaa".
+Operation 2: Removal of b's modifies str = "aaaaa".
+Operation 3: Removal of all remaining a's makes str 
+empty.
+Therefore, the minimum number of operations required 
+is 3.*/
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+int minSteps(string str) {
+    int count=1;
+    for(int i=1;i<str.size();i++)
+    if(str[i]!=str[i-1]) count++;
+    return count/2+1;
+}
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        cout << minSteps(str) << endl;
+    }
+}
+
+// } Driver Code Ends
