0b4da3170b2ac341415bae68b91215ed2ad4009b
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-04-03 17:53:25 +0530

    Update long_subsequence_dif_adjucents.cpp

--- Hashing/Dynamic_P/long_subsequence_dif_adjucents.cpp
+++ Hashing/Dynamic_P/long_subsequence_dif_adjucents.cpp
@@ -6,7 +6,6 @@ Output :  3
 As longest subsequences with difference 1 are, "10, 9, 8", 
 "4, 5, 4" and "4, 5, 6"
 
-
 Input :  arr[] = {1, 2, 3, 2, 3, 7, 2, 1}
 Output :  7
 As longest consecutive sequence is "1, 2, 3, 2, 3, 2, 1"*/
