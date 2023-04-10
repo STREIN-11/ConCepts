645528ed4f351e688084cdd0663686ae1716b19f
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-04-10 21:50:07 +0530

    Add files via upload

---
+++ Daily/maximum_intersecting_lines.cpp
@@ -0,0 +1,72 @@
+/*
+N horizontal line segments are arranged on the X-axis of a 2D plane. The start and end point of each line segment is given in a
+Nx2 matrix lines[ ][ ]. Your task is to find the maximum number of intersections possible of any vertical line with the 
+given N line segments.
+
+Example 1:
+Input:
+N = 4
+lines[][] = {{1,3}, {2,3}, {1,2}, {4,4}}
+Output:
+3
+Explanation:
+A vertical line at X = 2 passes through 
+{1,3}, {2,3}, {1,2}, ie three of the 
+given horizontal lines.
+
+Example 2:
+Input: 
+N = 3
+lines[][] = {{1, 3}, {5, 6}, {3,4}}
+Output:
+2
+Explanation: 
+A vertical line at X = 3 passes through 
+two of the given horizontal lines which 
+is the maximum possible.*/
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int maxIntersections(vector<vector<int>> lines, int N) {
+        map<int, int> m;
+        for(auto i : lines) m[i[0]]++, m[i[1] + 1]--;
+        int ans = 0, s = 0;
+        for(auto i : m) {
+            s += i.second;
+            ans = max(ans, s);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> mat(N, vector<int>(2));
+        for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < N; i++) {
+                cin >> mat[i][j];
+            }
+        }
+        Solution obj;
+        cout << obj.maxIntersections(mat, N) << endl;
+    }
+}
+// } Driver Code Ends
+
+
