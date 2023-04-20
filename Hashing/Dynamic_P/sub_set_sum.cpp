2f3fef65e1a597f3bef401ea4d37df6d0ffbf622
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-04-21 03:58:24 +0530

    Update sub_set_sum.cpp

--- Hashing/Dynamic_P/sub_set_sum.cpp
+++ Hashing/Dynamic_P/sub_set_sum.cpp
@@ -3,6 +3,19 @@ Input: set[] = {3, 34, 4, 12, 5, 2}, sum = 9
 Output: True  
 There is a subset (4, 5) with sum 9.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 Input: set[] = {3, 34, 4, 12, 5, 2}, sum = 30
 Output: False
 There is no subset that add up to 30.*/
