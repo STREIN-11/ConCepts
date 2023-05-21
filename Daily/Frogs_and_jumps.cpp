13da740bb66aeda532df3941a5701d9ea5fef272
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-05-21 23:25:52 +0530

    Update Frogs_and_jumps.cpp

--- Daily/Frogs_and_jumps.cpp
+++ Daily/Frogs_and_jumps.cpp
@@ -3,7 +3,6 @@ N frogs are positioned at one end of the pond. All frogs want to reach the other
 The pond has some leaves arranged in a straight line. Each frog has the strength to jump exactly K leaves. For example,
 a frog having strength 2 will visit the leaves 2, 4, 6, ...  etc. while crossing the pond.
 
-
 Given the strength of each frog and the number of leaves, your task is to find the number of leaves that not be visited by any 
 of the frogs when all frogs have reached the other end of the pond. 
 
