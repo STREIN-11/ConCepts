a2dbc0b482dff6862e0a0a45e27cd2736fb5456b
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-05-26 03:07:42 +0530

    Update Game_with_nos.c

--- Daily/Game_with_nos.c
+++ Daily/Game_with_nos.c
@@ -6,7 +6,6 @@ At index 0, arr[0] xor arr[1] = 1
 At index 1, arr[1] xor arr[2] = 10
 At index 2, arr[2] xor arr[3] = 3
 ...
-
 At index 4, No element is left So, it will remain as
 it is.
 New Array will be {1, 10, 3, 1, 3}.*/
