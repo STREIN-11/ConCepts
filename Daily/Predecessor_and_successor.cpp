f460b6483629407866e5a3cd3bd5a55cc9ad6710
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-06-06 21:55:26 +0530

    Update Predecessor_and_successor.cpp

--- Daily/Predecessor_and_successor.cpp
+++ Daily/Predecessor_and_successor.cpp
@@ -4,7 +4,6 @@ of a given key. If either predecessor or successor is not found, then set it to 
 
 
 
-
 Note:- In an inorder traversal the number just smaller than the target is the predecessor and the number just greater than the 
 target is the successor. 
 
