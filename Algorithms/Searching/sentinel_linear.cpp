f96dc69ba4dba33727d53859edb687fd5a19fa84
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-06-08 18:31:59 +0530

    Update sentinel_linear.cpp

---
+++ Algorithms/Searching/sentinel_linear.cpp
@@ -0,0 +1,36 @@
+/*https://www.geeksforgeeks.org/sentinel-linear-search/*/
+
+#include <iostream>
+using namespace std;
+
+void sentinelSearch(int arr[], int n, int key){
+	int last = arr[n - 1];
+	arr[n - 1] = key;
+	int i = 0;
+
+	while (arr[i] != key)
+		i++;
+	arr[n - 1] = last;
+
+	if ((i < n - 1) || (arr[n - 1] == key))
+		cout << key << " is present at index " << i;
+	else
+		cout << "Element Not found";
+}
+
+int main(){
+    int size,key;
+    cout<<"Enter Size : ";
+    cin>>size;
+	int arr[size];
+	// int n = sizeof(arr) / sizeof(arr[0]);
+    cout<<"Enter Array Elements : ";
+    for (int i = 0; i < size; i++){
+        cin>>arr[i];
+    }
+    cout<<"Enter The Key : ";
+    cin>>key;
+	sentinelSearch(arr, size, key);
+
+	return 0;
+}
