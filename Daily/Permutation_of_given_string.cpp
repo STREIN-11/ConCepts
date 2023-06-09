36f9dbdff72c2801ee454bb30d5a3f230a94d8d2
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-06-09 20:30:09 +0530

    Add files via upload

---
+++ Daily/Permutation_of_given_string.cpp
@@ -0,0 +1,63 @@
+/*
+Given a string S. The task is to print all unique permutations of the given string in lexicographically sorted order.
+
+Input: ABC
+Output:
+ABC ACB BAC BCA CAB CBA
+Explanation:
+Given string ABC has permutations in 6 
+forms as ABC, ACB, BAC, BCA, CAB and CBA .
+
+Input: ABSG
+Output:
+ABGS ABSG AGBS AGSB ASBG ASGB BAGS 
+BASG BGAS BGSA BSAG BSGA GABS GASB 
+GBAS GBSA GSAB GSBA SABG SAGB SBAG 
+SBGA SGAB SGBA
+Explanation:
+Given string ABSG has 24 permutations.*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+		vector<string>find_permutation(string S)
+		{
+		    string t;
+	        vector<string> v;
+		    t=S;
+		    sort(t.begin(),t.end());v.push_back(t);
+		    while(next_permutation(t.begin(),t.end()))
+		    {
+		        v.push_back(t);
+		    }
+		    return v;
+		}
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--)
+    {
+	    string S;
+	    cin >> S;
+	    Solution ob;
+	    vector<string> ans = ob.find_permutation(S);
+	    for(auto i: ans)
+	    {
+	    	cout<<i<<" ";
+	    }
+	    cout<<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
