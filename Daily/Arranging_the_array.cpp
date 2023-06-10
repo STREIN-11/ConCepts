8742fa48213f6fc408da385c3725c1e392627afe
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-06-10 21:25:26 +0530

    Add files via upload

---
+++ Daily/Arranging_the_array.cpp
@@ -0,0 +1,79 @@
+/*
+You are given an array of size N. Rearrange the given array in-place such that all the negative numbers occur before all non-negative numbers.
+(Maintain the order of all -ve and non-negative numbers as given in the original array).
+
+Input:
+N = 4
+Arr[] = {-3, 3, -2, 2}
+Output:
+-3 -2 3 2
+Explanation:
+In the given array, negative numbers
+are -3, -2 and non-negative numbers are 3, 2. 
+
+Input:
+N = 4
+Arr[] = {-3, 1, 0, -2}
+Output:
+-3 -2 1 0
+Explanation:
+In the given array, negative numbers
+are -3, -2 and non-negative numbers are 1, 0.*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        void Rearrange(int arr[], int n)
+        {
+            stack<int> s;
+            int neg =0;
+            for(int i=n-1 ;i>=0;i--){
+                if( arr[i] <0)neg++;
+                s.push(arr[i]);
+            }
+            int m=0;
+            while(!s.empty()){
+                if( s.top()<0){
+                    arr[m]=s.top();
+                    m++;
+                }
+                else{
+                    arr[neg]=s.top();
+                    neg++;
+                }
+                s.pop();
+            }
+        }
+};
+
+//{ Driver Code Starts.
+void Rearrange(int arr[], int n);
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
+        long long j=0;
+        Solution ob;
+        ob.Rearrange(arr, n);
+      
+        for (int i = 0; i < n; i++) 
+            cout << arr[i] << " "; 
+        cout << endl;  
+    }
+    return 0; 
+} 
+// } Driver Code Ends
