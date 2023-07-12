468106bb48420608c1f5ddc366fc2141be2d63c3
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-07-13 01:45:56 +0530

    Add files via upload

---
+++ Daily/unique_number_of_occurence.cpp
@@ -0,0 +1,75 @@
+/*
+Given an array arr of N integers, the task is to check whether the frequency of the elements in the array 
+is unique or not. Or in other words, there are no two distinct numbers in array with equal frequency. If all the 
+frequency is unique then return true, else return false.
+
+Input:
+N = 5
+arr = [1, 1, 2, 5, 5]
+Output:
+false
+Explanation:
+The array contains 2 (1’s), 1 (2’s) and 2 (5’s), since the number of frequency of 1 and 5 are the same i.e. 2 times. 
+Therefore, this array does not satisfy the condition.
+
+Input:
+N = 10
+arr = [2, 2, 5, 10, 1, 2, 10, 5, 10, 2]
+Output:
+true
+Explanation:
+Number of 1’s -> 1
+Number of 2’s -> 4
+Number of 5’s -> 2
+Number of 10’s -> 3.
+Since, the number of occurrences of elements present in the array is unique. Therefore, this array satisfy the condition.*/
+
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    bool isFrequencyUnique(int n, int arr[])
+    {map<int,int>mp,mp1;
+        
+        
+        for(int i=0;i<n;i++){
+           mp[arr[i]]++;
+        }
+        for(auto i:mp){
+            if(mp1[i.second] == false)
+            mp1[i.second] = true;
+            else
+            return false;
+        }
+        
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        bool ans=ob.isFrequencyUnique(n,arr);
+        if(ans)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+}
+// } Driver Code Ends
