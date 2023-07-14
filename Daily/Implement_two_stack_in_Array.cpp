f9017d81e5a89622322024d14f9f3f4cba68b6cb
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-07-14 21:18:53 +0530

    Create Implement_two_stack_in_Array.cpp

---
+++ Daily/Implement_two_stack_in_Array.cpp
@@ -0,0 +1,136 @@
+/*
+Your task is to implement  2 stacks in one array efficiently. You need to implement 4 methods.
+push1 : pushes element into first stack.
+push2 : pushes element into second stack.
+pop1 : pops element from first stack and returns the popped element. If first stack is empty, it should return -1.
+pop2 : pops element from second stack and returns the popped element. If second stack is empty, it should return -1.
+
+Input:
+push1(2)
+push1(3)
+push2(4)
+pop1()
+pop2()
+pop2()
+Output:
+3 4 -1
+Explanation:
+push1(2) the stack1 will be {2}
+push1(3) the stack1 will be {2,3}
+push2(4) the stack2 will be {4}
+pop1()   the poped element will be 3 from stack1 and stack1 will be {2}
+pop2()   the poped element will be 4 from stack2 and now stack2 is empty
+pop2()   the stack2 is now empty hence returned -1.
+
+Input:
+push1(1)
+push2(2)
+pop1()
+push1(3)
+pop1()
+pop1()
+Output:
+3 1 -1
+Explanation:
+push1(1) the stack1 will be {1}
+push2(2) the stack2 will be {2}
+pop1()   the poped element will be 1 from stack1 and stack1 will be empty
+push1(3) the stack1 will be {3}
+pop1()   the poped element will be 3 from stack1 and stack1 will be empty
+pop1()   the stack1 is now empty hence returned -1.*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class twoStacks
+{
+    int *arr;
+    int size;
+    int top1, top2;
+    public:
+    
+    twoStacks(int n=100)
+    {
+        size = n; 
+        arr = new int[n]; 
+        top1 = -1; 
+        top2 = size;
+    }
+ 
+    //Function to push an integer into the stack1.
+    void push1(int x)
+    {if(top1 <top2)
+        arr[++top1] =x;
+    }
+    
+    //Function to push an integer into the stack2.
+    void push2(int x)
+    {
+       if(top1 <top2)arr[--top2]=x;
+    }
+    
+    //Function to remove an element from top of the stack1.
+    int pop1()
+    {
+      if(top1>(-1))
+      return arr[top1--];
+      return -1;
+    }
+    
+    //Function to remove an element from top of the stack2.
+    int pop2()
+    {
+       if(top2 <size)
+       return arr[top2++];
+       return -1;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        twoStacks *sq = new twoStacks();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int stack_no;
+        cin>>stack_no;
+        int QueryType=0;
+        cin>>QueryType;
+        
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            if(stack_no ==1)
+            sq->push1(a);
+            else if(stack_no==2)
+            sq->push2(a);
+        }else if(QueryType==2){
+        	if(stack_no==1)
+            cout<<sq->pop1()<<" ";
+            else if(stack_no==2)
+            cout<<sq->pop2()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+}
+
+// } Driver Code Ends
