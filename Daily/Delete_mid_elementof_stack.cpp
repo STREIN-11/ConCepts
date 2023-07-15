2f60f44740329075981caa299a95a2ebc7f7309f
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-07-15 18:56:40 +0530

    Add files via upload

---
+++ Daily/Delete_mid_elementof_stack.cpp
@@ -0,0 +1,81 @@
+/*
+Given a stack, delete the middle element of the stack without using any additional data structure.
+Middle element:- ceil((size_of_stack+1)/2) (1-based indexing) from bottom of the stack.
+
+Note: The output shown by the compiler is the stack from top to bottom.
+
+Input: 
+Stack = {10, 20, 30, 40, 50}
+Output:
+ModifiedStack = {10, 20, 40, 50}
+Explanation:
+If you print the stack the bottom-most element will be 10 and the top-most element will be 50. Middle element 
+will be element at index 3 from bottom, which is 30. Deleting 30, stack will look like {10 20 40 50}.
+
+Input: 
+Stack = {10 20 30 40}
+Output:
+ModifiedStack = {10 30 40}
+Explanation:
+If you print the stack the bottom-most element will be 10 and the top-most element will be 40. 
+Middle element will be element at index 2 from bottom, which is 20. Deleting 20, stack will look like {10 30 40}.*/
+
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+    public:
+    //Function to delete middle element of a stack.
+    void deleteMid(stack<int>&s, int sizeOfStack)
+    {
+        if(s.size()==(sizeOfStack+1)/2){
+            s.pop();
+            return;
+        }
+        int x=s.top();
+        s.pop();
+        deleteMid(s,sizeOfStack);
+        s.push(x);
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int sizeOfStack;
+        cin>>sizeOfStack;
+        
+        stack<int> myStack;
+        
+        for(int i=0;i<sizeOfStack;i++)
+        {
+            int x;
+            cin>>x;
+            myStack.push(x);    
+        }
+
+            Solution ob;
+            ob.deleteMid(myStack,myStack.size());
+            while(!myStack.empty())
+            {
+                cout<<myStack.top()<<" ";
+                myStack.pop();
+            }
+        cout<<endl;
+    }   
+    return 0;
+}
+
+// } Driver Code Ends
