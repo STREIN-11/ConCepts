67eb8bf2656e316c66379847f985547e6d7257c2
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-07-18 02:21:32 +0530

    Add files via upload

---
+++ Daily/Longest_Reapeting_subSequence.cpp
@@ -0,0 +1,102 @@
+/*
+Given string str, find the length of the longest repeating subsequence such that it can be found twice in the given string.
+
+The two identified subsequences A and B can use the same ith character from string str if and only if that ith 
+character has different indices in A and B. For example, A = "xax" and B = "xax" then the index of first "x" must be 
+different in the original string for A and B.
+
+Input:
+str = "axxzxy"
+Output: 2
+Explanation:
+The given array with indexes looks like
+a x x z x y 
+0 1 2 3 4 5
+
+The longest subsequence is "xx". 
+It appears twice as explained below.
+
+subsequence A
+x x
+0 1  <-- index of subsequence A
+------
+1 2  <-- index of str 
+
+
+subsequence B
+x x
+0 1  <-- index of subsequence B
+------
+2 4  <-- index of str 
+
+We are able to use character 'x' 
+(at index 2 in str) in both subsequences
+as it appears on index 1 in subsequence A 
+and index 0 in subsequence B.
+
+
+Example:2 
+Input:
+str = "axxxy"
+Output: 2
+Explanation:
+The given array with indexes looks like
+a x x x y 
+0 1 2 3 4
+
+The longest subsequence is "xx". 
+It appears twice as explained below.
+
+subsequence A
+x x
+0 1  <-- index of subsequence A
+------
+1 2  <-- index of str 
+
+
+subsequence B
+x x
+0 1  <-- index of subsequence B
+------
+2 3  <-- index of str 
+
+We are able to use character 'x' 
+(at index 2 in str) in both subsequences
+as it appears on index 1 in subsequence A 
+and index 0 in subsequence B.*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+	public:
+		int LongestRepeatingSubsequence(string str){
+		    int n = str.size();
+	    vector<int> prev(n+1), curr(n+1);
+	    for(int i = 1; i <= n; i++) {
+	        for(int j = 1; j <= n; j++) {
+	            if(str[i-1] == str[j-1] && i != j) curr[j] = 1 + prev[j-1];
+	            else curr[j] = max(prev[j], curr[j-1]);
+	        }
+	        prev = curr;
+	    }
+	    return curr[n];
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string str;
+		cin >> str;
+		Solution obj;
+		int ans = obj.LongestRepeatingSubsequence(str);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
