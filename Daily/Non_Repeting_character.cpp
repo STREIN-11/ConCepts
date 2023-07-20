e05ea4d00322858a3d3aac3f56304b90710a3dd1
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-07-20 22:36:34 +0530

    Create Non_Repeting_character.cpp

---
+++ Daily/Non_Repeting_character.cpp
@@ -0,0 +1,67 @@
+/*
+Given a string S consisting of lowercase Latin Letters. Return the first non-repeating character in S. If there is no non-repeating character, return '$'.
+Example 1:
+Input:
+S = hello
+Output: h
+Explanation: In the given string, the
+first character which is non-repeating
+is h, as it appears first and there is
+no other 'h' in the string.
+
+Example 2:
+Input:
+S = zxvczbtxyzvy
+Output: c
+Explanation: In the given string, 'c' is
+the character which is non-repeating. */
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find the first non-repeating character in a string.
+    char nonrepeatingCharacter(string S)
+    {
+       //Your code here
+       unordered_map<char,int>freq;
+      for(char c:S) freq[c]++;
+      for(char c:S) if(freq[c]==1)return c;
+      return '$';
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int T;
+	cin >> T;
+	
+	while(T--)
+	{
+	
+	    string S;
+	    cin >> S;
+	    Solution obj;
+        char ans = obj.nonrepeatingCharacter(S);
+        
+        if(ans != '$')
+	    cout << ans;
+        else cout << "-1";
+            
+        cout << endl;
+	    
+	}
+	
+	return 0;
+}
+
+// } Driver Code Ends
