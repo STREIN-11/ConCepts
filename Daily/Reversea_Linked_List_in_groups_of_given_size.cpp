6a64e0e8b4458aba41b773b7bf5e4bd1a95e853b
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-07-21 00:07:23 +0530

    Create Reversea_Linked_List_in_groups_of_given_size.cpp

---
+++ Daily/Reversea_Linked_List_in_groups_of_given_size.cpp
@@ -0,0 +1,141 @@
+/*
+Given a linked list of size N. The task is to reverse every k nodes (where k is an input to the function) in the linked list. If the number 
+of nodes is not a multiple of k then left-out nodes, in the end, should be considered as a group and must be reversed (See Example 2 for clarification).
+
+Input:
+LinkedList: 1->2->2->4->5->6->7->8
+K = 4
+Output: 4 2 2 1 8 7 6 5 
+Explanation: 
+The first 4 elements 1,2,2,4 are reversed first 
+and then the next 4 elements 5,6,7,8. Hence, the 
+resultant linked list is 4->2->2->1->8->7->6->5.
+
+Input:
+LinkedList: 1->2->3->4->5
+K = 3
+Output: 3 2 1 5 4 
+Explanation: 
+The first 3 elements are 1,2,3 are reversed 
+first and then elements 4,5 are reversed.Hence, 
+the resultant linked list is 3->2->1->5->4.*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct node
+{
+    int data;
+    struct node* next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+/* Function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+/*
+  Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+    struct node
+    {
+        int data;
+        struct node* next;
+    
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+    
+    }*head;
+*/
+
+class Solution
+{
+    public:
+    struct node *reverse (struct node *head, int k)
+    { 
+        if(!head) return NULL; 
+        node* prev = NULL;
+        node* curr = head;
+        node* nxt=NULL;
+        int count = 0;
+        while(curr && count<k){
+            nxt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
+            count++;
+        }
+        
+        if(nxt){
+            head->next = reverse(nxt,k);
+        }
+        
+        return prev;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Drier program to test above function*/
+int main(void)
+{
+    int t;
+    cin>>t;
+     
+    while(t--)
+    {
+        struct node* head = NULL;
+        struct node* temp = NULL;
+        int n;
+        cin >> n;
+         
+        for(int i=0 ; i<n ; i++)
+        {
+            int value;
+            cin >> value;
+            if(i == 0)
+            {
+                head = new node(value);
+                temp = head;
+            }
+            else
+            {
+                temp->next = new node(value);
+                temp = temp->next;
+            }
+        }
+        
+        int k;
+        cin>>k;
+        
+        Solution ob;
+        head = ob.reverse(head, k);
+        printList(head);
+    }
+     
+    return(0);
+}
+
+
+// } Driver Code Ends
