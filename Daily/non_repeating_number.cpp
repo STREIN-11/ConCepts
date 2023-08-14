807a59020ebb4c598c6c84f5b4289edd754c0937
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-08-14 18:17:26 +0530

    Update non_repeating_number.cpp

--- Daily/non_repeating_number.cpp
+++ Daily/non_repeating_number.cpp
@@ -5,7 +5,6 @@ Given an array A containing 2*N+2 positive numbers, out of which 2*N numbers exi
 occur exactly once and are distinct. Find the other two numbers. Return in increasing order.
 
 Example 1:
-
 Input: 
 N = 2
 arr[] = {1, 2, 3, 2, 1, 4}
