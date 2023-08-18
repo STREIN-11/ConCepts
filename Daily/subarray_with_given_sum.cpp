87ee2ce769ed949784ef64b98c572faf64eb899b
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-08-19 03:12:49 +0530

    Update subarray_with_given_sum.cpp

--- Daily/subarray_with_given_sum.cpp
+++ Daily/subarray_with_given_sum.cpp
@@ -2,7 +2,6 @@
 Given an unsorted array A of size N that contains only positive integers, find a continuous sub-array that adds to a given number S and return 
 the left and right index(1-based indexing) of that subarray.
 In case of multiple subarrays, return the subarray indexes which come first on moving from left to right.
-
 Note:- You have to return an ArrayList consisting of two elements left and right. In case no such subarray exists return an array consisting of element -1.
 
 Example 1:
