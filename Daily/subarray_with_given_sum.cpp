78587f2d34d28740f0c4eab3d5ed121ebd1f7ea8
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-08-19 03:10:10 +0530

    Create subarray_with_given_sum.cpp

---
+++ Daily/subarray_with_given_sum.cpp
@@ -0,0 +1,82 @@
+/*
+Given an unsorted array A of size N that contains only positive integers, find a continuous sub-array that adds to a given number S and return 
+the left and right index(1-based indexing) of that subarray.
+  
+In case of multiple subarrays, return the subarray indexes which come first on moving from left to right.
+
+Note:- You have to return an ArrayList consisting of two elements left and right. In case no such subarray exists return an array consisting of element -1.
+
+Example 1:
+
+Input:
+N = 5, S = 12
+A[] = {1,2,3,7,5}
+Output: 2 4
+Explanation: The sum of elements 
+from 2nd position to 4th position 
+is 12.
+
+Example 2:
+
+Input:
+N = 10, S = 15
+A[] = {1,2,3,4,5,6,7,8,9,10}
+Output: 1 5
+Explanation: The sum of elements 
+from 1st position to 5th position
+is 15.*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(vector<int>arr, int n, long long s)
+    {
+        long long sum = 0;
+        for(int i = 0, j = 0; i < n && s != 0; i++){
+            sum += arr[i];
+            while(sum > s)
+                sum -= arr[j++];
+            if(sum == s)
+                return {j + 1, i + 1};                
+        }
+        return {-1};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        long long s;
+        cin>>n>>s;
+        vector<int>arr(n);
+        // int arr[n];
+        const int mx = 1e9;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        vector<int>res;
+        res = ob.subarraySum(arr, n, s);
+        
+        for(int i = 0;i<res.size();i++)
+            cout<<res[i]<<" ";
+        cout<<endl;
+        
+    }
+	return 0;
+}
+// } Driver Code Ends
