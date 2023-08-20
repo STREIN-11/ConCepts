695ef3fca6b8cc00c908ba90d598c77460e4b391
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-08-20 22:55:45 +0530

    Create Number_of_occurrence.cpp

---
+++ Daily/Number_of_occurrence.cpp
@@ -0,0 +1,70 @@
+/*
+Given a sorted array Arr of size N and a number X, you need to find the number of occurrences of X in Arr.
+
+Example 1:
+
+Input:
+N = 7, X = 2
+Arr[] = {1, 1, 2, 2, 2, 2, 3}
+Output: 4
+Explanation: 2 occurs 4 times in the
+given array.
+
+Example 2:
+
+Input:
+N = 7, X = 4
+Arr[] = {1, 1, 2, 2, 2, 2, 3}
+Output: 0
+Explanation: 4 is not present in the
+given array.*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	int count(int arr[], int n, int x) {
+	    int count=0;
+        auto it = find(arr , arr+n ,x);
+        if(it != arr+n)
+        {
+            while(arr[it-arr] == x)
+            {
+                count++;
+                it++;
+            }
+           return count;
+        }
+        else
+            return 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.count(arr, n, x);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
