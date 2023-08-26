b5f8750c1e607df44f9ed5d365f52a1b42a06d1e
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-08-27 02:46:33 +0530

    Create Reverse_string.cpp

---
+++ Daily/Reverse_string.cpp
@@ -0,0 +1,66 @@
+/*
+You are given a string s. You need to reverse the string.
+
+Example 1:
+
+Input:
+s = Geeks
+Output: skeeG
+
+Example 2:
+
+Input:
+s = for
+Output: rof*/
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        stack<char>s;
+        string ans;
+        for(auto i:str){
+            s.push(i);
+        }
+       while(!s.empty()){
+            ans.push_back(s.top());
+            s.pop();
+        }
+        return ans;
+        
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+    	string s;
+    	cin >> s;
+    	Solution ob;
+    	cout << ob.reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
