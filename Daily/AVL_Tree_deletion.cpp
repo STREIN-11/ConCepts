ea1063ba63b1f5146f81c6bdf080c15cebadb47a
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-08-31 15:05:47 +0530

    Add files via upload

---
+++ Daily/AVL_Tree_deletion.cpp
@@ -0,0 +1,267 @@
+/*
+Given an AVL tree and N values to be deleted from the tree. Write a function to delete a given value from the tree. 
+All the N values which needs to be deleted are passed one by one as input data by driver code itself, you are asked to 
+return the root of modified tree after deleting the value.
+
+Example 1:
+
+Tree = 
+        4
+      /   \
+     2     6
+    / \   / \  
+   1   3 5   7
+
+N = 4
+Values to be deleted = {4,1,3,6}
+
+Input: Value to be deleted = 4
+Output:
+        5    
+      /   \
+     2     6
+    / \     \  
+   1   3     7
+
+Input: Value to be deleted = 1
+Output:
+        5    
+      /   \
+     2     6
+      \     \  
+       3     7
+
+Input: Value to be deleted = 3
+Output:
+        5    
+      /   \
+     2     6
+            \  
+             7
+
+Input: Value to be deleted = 6
+Output:
+        5    
+      /   \
+     2     7
+*/
+
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data, height;
+	Node *left, *right;
+	Node(int x)
+	{
+		data = x;
+		height = 1;
+		left = right = NULL;
+	}
+};
+
+int setHeights(Node* n)
+{
+	if(!n) return 0;
+	n->height = 1 + max( setHeights(n->left) , setHeights(n->right) );
+	return n->height;
+}
+
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    setHeights(root);
+    return root;
+}
+
+bool isBST(Node *n, int lower, int upper)
+{
+	if(!n) return 1;
+	if( n->data <= lower || n->data >= upper ) return 0;
+	return isBST(n->left, lower, n->data) && isBST(n->right, n->data, upper) ;
+}
+
+pair<int,bool> isBalanced(Node* n)
+{
+	if(!n) return pair<int,bool> (0,1);
+
+	pair<int,bool> l = isBalanced(n->left);
+	pair<int,bool> r = isBalanced(n->right);
+
+	if( abs(l.first - r.first) > 1 ) return pair<int,bool> (0,0);
+
+	return pair<int,bool> ( 1 + max(l.first , r.first) , l.second && r.second );
+}
+
+bool isBalancedBST(Node* root)
+{
+	if( !isBST(root, INT_MIN, INT_MAX) )
+		cout<< "BST voilated, inorder traversal : ";
+
+	else if ( ! isBalanced(root).second )
+		cout<< "Unbalanced BST, inorder traversal : ";
+
+	else return 1;
+	return 0;
+}
+
+void printInorder(Node* n)
+{
+	if(!n) return;
+	printInorder(n->left);
+	cout<< n->data << " ";
+	printInorder(n->right);
+}
+
+struct Node* deleteNode(struct Node* root, int data);
+
+int main()
+{
+	int t;
+	cin>>t;
+	getchar();
+
+	while(t--)
+	{
+		string s;
+		getline(cin,s);
+		Node* root = buildTree(s);
+        
+		int n;
+		cin>> n;
+		int ip[n];
+		for(int i=0; i<n; i++)
+			cin>> ip[i];
+        
+		for(int i=0; i<n; i++)
+		{
+			root = deleteNode(root, ip[i]);
+			
+			if( !isBalancedBST(root) )
+				break;
+		}
+        
+		if(root==NULL)
+			cout<<"null";
+		else
+			printInorder(root);
+		cout<< endl;
+        
+		getline(cin,s); // to deal with newline char
+	}
+	return 1;
+}
+
+// } Driver Code Ends
+
+
+/* Node is as follows:
+
+struct Node
+{
+	int data, height;
+	Node *left, *right;
+	Node(int x)
+	{
+		data = x;
+		height = 1;
+		left = right = NULL;
+	}
+};
+
+*/
+
+void getElements(Node* root, vector<int>& inorder, int data){
+    if(!root)
+        return;
+    getElements(root -> left, inorder, data);
+    if(data != root -> data)
+        inorder.push_back(root -> data);
+    getElements(root -> right, inorder, data);
+}
+
+Node* getBalancedTree(vector<int> elements, int height){
+    if(elements.size() == 0){
+        return NULL;
+    }
+    int mid = elements.size() >> 1;
+    Node* temp = new Node(elements[mid]);
+    temp -> height = height;
+    if(elements.size() == 1){
+        
+        return temp;
+    }
+    vector<int> left(elements.begin(), elements.begin() + mid), right(elements.begin() + mid + 1, elements.end());
+    temp -> left = getBalancedTree(left, height + 1);
+    temp -> right = getBalancedTree(right, height + 1);
+    return temp;
+    
+}
+
+Node* deleteNode(Node* root, int data)
+{
+    vector<int> inorder;
+    getElements(root, inorder, data);
+    return getBalancedTree(inorder, 1);
+}
