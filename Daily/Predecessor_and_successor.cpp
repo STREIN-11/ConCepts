b8c07db5035f5a9ad95dd8b19be8c1e2b5e53a32
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-09-17 09:02:35 +0530

    Update Predecessor_and_successor.cpp

--- Daily/Predecessor_and_successor.cpp
+++ Daily/Predecessor_and_successor.cpp
@@ -2,6 +2,8 @@
 There is BST given with the root node with the key part as an integer only. You need to find the in-order successor and predecessor
 of a given key. If either predecessor or successor is not found, then set it to NULL.
 
+
+
 Note:- In an inorder traversal the number just smaller than the target is the predecessor and the number just greater than the 
 target is the successor. 
 
