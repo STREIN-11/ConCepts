e091526a02e2d3f4bd3c4e24feb9092699abe26e
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-09-20 20:39:14 +0530

    Update coin_change.cpp

--- Daily/coin_change.cpp
+++ Daily/coin_change.cpp
@@ -4,7 +4,6 @@ by using different combinations from coins[ ].
 Note: Assume that you have an infinite supply of each type of coin. And you can use any coin as many times as you want.
 
 
-
 Input:
 N = 3, sum = 4
 coins = {1,2,3}
