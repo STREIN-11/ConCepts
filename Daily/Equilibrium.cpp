8b8906abc4ef98634e1fdb278b09120c04ff412a
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-09-23 21:22:37 +0530

    Create Equilibrium.cpp

---
+++ Daily/Equilibrium.cpp
@@ -0,0 +1,80 @@
+/*
+Given an array A of n positive numbers. The task is to find the first equilibrium point in an array. Equilibrium point in an array is a position 
+such that the sum of elements before it is equal to the sum of elements after it.
+
+Note: Return equilibrium point in 1-based indexing. Return -1 if no such point exists. 
+
+Example 1:
+Input: 
+n = 5 
+A[] = {1,3,5,2,2} 
+Output: 
+3 
+Explanation:  
+equilibrium point is at position 3 as sum of elements before it (1+3) = sum of elements after it (2+2). 
+
+Example 2:
+Input:
+n = 1
+A[] = {1}
+Output: 
+1
+Explanation:
+Since there's only element hence its only the equilibrium point.*/
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+    
+        if(n==1)return 1;
+        
+        for(int i=1;i<n;i++)a[i]+=a[i-1];
+        
+        for(int i=1;i<n-1;i++){
+            if(a[i-1]==(a[n-1]-a[i]))return i+1;
+        }
+        
+        return -1;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+
+    long long t;
+    
+    //taking testcases
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        
+        //taking input n
+        cin >> n;
+        long long a[n];
+
+        //adding elements to the array
+        for (long long i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        
+        Solution ob;
+
+        //calling equilibriumPoint() function
+        cout << ob.equilibriumPoint(a, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
