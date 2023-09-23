2734a37ae037c59abf1f9597e7b62466c7cf7af2
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-09-23 21:24:19 +0530

    Update Equilibrium.cpp

--- Daily/Equilibrium.cpp
+++ Daily/Equilibrium.cpp
@@ -26,22 +26,14 @@ Since there's only element hence its only the equilibrium point.*/
 using namespace std;
 class Solution{
     public:
-    // Function to find equilibrium point in the array.
-    // a: input array
-    // n: size of array
     int equilibriumPoint(long long a[], int n) {
-    
         if(n==1)return 1;
-        
         for(int i=1;i<n;i++)a[i]+=a[i-1];
-        
         for(int i=1;i<n-1;i++){
             if(a[i-1]==(a[n-1]-a[i]))return i+1;
         }
-        
         return -1;
     }
-
 };
 
 //{ Driver Code Starts.
