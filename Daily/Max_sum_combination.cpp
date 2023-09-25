2ed770b4f75a0f44bf3e9cf4d97c38e5c6bb92e1
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-09-25 14:33:02 +0530

    Add files via upload

---
+++ Daily/Max_sum_combination.cpp
@@ -0,0 +1,89 @@
+/*
+Given two integer array A and B of size N each.
+A sum combination is made by adding one element from array A and another element of array B.
+Return the maximum K valid sum combinations from all the possible sum combinations.
+
+Note : Output array must be sorted in non-increasing order.
+
+Example 1:
+
+Input:
+N = 2
+K = 2
+A [ ] = {3, 2}
+B [ ] = {1, 4}
+Output: {7, 6}
+Explanation: 
+7 -> (A : 3) + (B : 4)
+6 -> (A : 2) + (B : 4)
+
+Example 2:
+
+Input:
+N = 4
+K = 3
+A [ ] = {1, 4, 2, 3}
+B [ ] = {2, 5, 1, 6}
+Output: {10, 9, 9}
+Explanation: 
+10 -> (A : 4) + (B : 6)
+9 -> (A : 4) + (B : 5)
+9 -> (A : 3) + (B : 6)*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> maxCombinations(int N, int K, vector<int> &A, vector<int> &B) {
+        sort(A.begin(),A.end());
+        sort(B.begin(),B.end());
+        
+        vector<int>ans;
+        priority_queue<pair<int,int>>pq;
+        for(int i=0;i<N;i++)
+        {
+            int sum = A[i]+B[N-1];
+            pq.push({sum,N-1});
+        }
+        
+        while(!pq.empty() and K--)
+        {
+            int sum = pq.top().first;
+            int idx = pq.top().second;
+            pq.pop();
+            ans.push_back(sum);
+            if(idx-1>=0)
+            pq.push({sum-B[idx]+B[idx-1], idx-1});
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        cin >> N >> K;
+
+        vector<int> A(N), B(N);
+        for (int i = 0; i < N; i++) {
+            cin >> A[i];
+        }
+        for (int i = 0; i < N; i++) {
+            cin >> B[i];
+        }
+        Solution obj;
+        vector<int> ans = obj.maxCombinations(N, K, A, B);
+        for (auto &it : ans) cout << it << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
