69557753f8a842ed87f3345ebd11d1c835a5351f
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-09-30 21:13:58 +0530

    Add files via upload

---
+++ Daily/Booloean_Matrix.cpp
@@ -0,0 +1,118 @@
+/*
+Given a boolean matrix of size RxC where each cell contains either 0 or 1, 
+modify it such that if a matrix cell matrix[i][j] is 1 then all the cells in its ith 
+row and jth column will become 1.
+
+Example 1:
+
+Input:
+R = 2, C = 2
+matrix[][] = {{1, 0},
+              {0, 0}}
+Output: 
+1 1
+1 0 
+Explanation:
+Only cell that has 1 is at (0,0) so all 
+cells in row 0 are modified to 1 and all 
+cells in column 0 are modified to 1.
+
+
+Example 2:
+
+Input:
+R = 4, C = 3
+matrix[][] = {{ 1, 0, 0},
+              { 1, 0, 0},
+              { 1, 0, 0},
+              { 0, 0, 0}}
+Output: 
+1 1 1
+1 1 1
+1 1 1
+1 0 0 
+Explanation:
+The position of cells that have 1 in
+the original matrix are (0,0), (1,0)
+and (2,0). Therefore, all cells in row
+0,1,2 are and column 0 are modified to 1. */
+
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+
+
+class Solution
+{   
+    public:
+    //Function to modify the matrix such that if a matrix cell matrix[i][j]
+    //is 1 then all the cells in its ith row and jth column will become 1.
+    void booleanMatrix(vector<vector<int> > &matrix)
+    {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        unordered_map<int,int> row,col;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==1){
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        for(auto x:row){
+            int i = x.first;
+            for(int j=0;j<m;j++)
+                matrix[i][j] = 1;
+        }
+        
+        for(auto x:col){
+            int j = x.first;
+            for(int i=0;i<n;i++)
+                matrix[i][j] = 1;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int row, col;
+        cin>> row>> col;
+        vector<vector<int> > matrix(row); 
+        for(int i=0; i<row; i++)
+        {
+            matrix[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        
+        Solution ob;
+        ob.booleanMatrix(matrix);
+
+
+        for (int i = 0; i < row; ++i)
+        {
+            for (int j = 0; j < col; ++j)
+            {
+                cout<<matrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
