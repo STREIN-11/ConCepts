468fc37332a84021166a6d950f5a465ccf02095b
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-10-17 19:55:40 +0530

    Update Fillup_buckets.cpp

--- Daily/Fillup_buckets.cpp
+++ Daily/Fillup_buckets.cpp
@@ -2,6 +2,7 @@
 Given n buckets and infinite number of balls. The maximum capacity of each bucket is given in an array capacity[]. 
 Find the number of ways to fill the buckets with balls such that each bucket has atleast 1 ball and all the buckets have distinct number
  of balls in them.Note: Since the answer may be very large, calculate the answer modulo 10^9+7.
+ 
 Input: 
 n = 1
 capacity = [6]
