f0f903f57dd076a6e5921351be2160e40f0f0ea8
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-10-19 20:49:23 +0530

    Update Givena_linked_list_of_0s_1s_and_2s_sort_it.cpp

--- Daily/Givena_linked_list_of_0s_1s_and_2s_sort_it.cpp
+++ Daily/Givena_linked_list_of_0s_1s_and_2s_sort_it.cpp
@@ -2,6 +2,13 @@
 Given a linked list of N nodes where nodes can contain values 0s, 1s, and 2s only. The task is to segregate 0s, 1s, and 2s 
 linked list such that all zeros segregate to head side, 2s at the end of the linked list, and 1s in the mid of 0s and 2s.
 
+
+
+
+
+
+
+
 Input:
 N = 8
 value[] = {1,2,2,1,2,0,2,2}
