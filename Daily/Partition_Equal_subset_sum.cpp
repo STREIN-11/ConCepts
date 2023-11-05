7fe16ef3171682a9b3b97375aac08aac2a8d0a4a
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-11-05 19:38:03 +0530

    Update Partition_Equal_subset_sum.cpp

--- Daily/Partition_Equal_subset_sum.cpp
+++ Daily/Partition_Equal_subset_sum.cpp
@@ -3,7 +3,6 @@ Given an array arr[] of size N, check if it can be partitioned into two parts su
 sum of elements in both parts is the same.
 
 Example 1:
-
 Input: N = 4
 arr = {1, 5, 11, 5}
 Output: YES
