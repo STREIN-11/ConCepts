5803869b3e337e74e920e073c2a43005a81010ce
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-11-21 21:20:43 +0530

    Update Non_Repeting_character.cpp

--- Daily/Non_Repeting_character.cpp
+++ Daily/Non_Repeting_character.cpp
@@ -1,5 +1,6 @@
 /*
 Given a string S consisting of lowercase Latin Letters. Return the first non-repeating character in S. If there is no non-repeating character, return '$'.
+
 Example 1:
 Input:
 S = hello
