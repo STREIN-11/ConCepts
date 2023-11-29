cffb9ab0635adc1823152cb9d78f424df8b8c54a
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2023-11-29 19:18:35 +0530

    Update Minimum_integer.cpp

--- Daily/Minimum_integer.cpp
+++ Daily/Minimum_integer.cpp
@@ -1,7 +1,6 @@
 /*
 You are given an array A of size N. Let us denote S as the sum of all integers present in the array. Among all integers present 
 in the array, find the minimum integer X such that S ≤ N*X.
-
 Input:
 N = 3,
 A = { 1, 3, 2}
