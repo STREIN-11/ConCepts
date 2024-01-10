2efd897db4d178937900b9628927a7812ad5e62b
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2024-01-10 20:53:45 +0530

    Update duplicate_in_array.cpp

--- Daily/duplicate_in_array.cpp
+++ Daily/duplicate_in_array.cpp
@@ -2,7 +2,6 @@
 Given an array a of size N which contains elements from 0 to N-1, you need to find all the 
 elements occurring more than once in the given array. Return the answer in ascending order. 
 If no such element is found, return list containing [-1]. 
-
 Note: The extra space is only for the array to be returned. Try and perform all operations 
 within the provided array. 
 
