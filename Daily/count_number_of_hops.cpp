e54b5b6a8f2700d81ffa569b4acd25bca1c569b7
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2024-05-20 22:46:01 +0530

    Update count_number_of_hops.cpp

--- Daily/count_number_of_hops.cpp
+++ Daily/count_number_of_hops.cpp
@@ -2,7 +2,6 @@
 A frog jumps either 1, 2, or 3 steps to go to the top. In how many ways can it reach the top of 
 Nth step. As the answer will be large find the answer modulo 1000000007.
 
-
 Example 1:
 Input:
 N = 1
