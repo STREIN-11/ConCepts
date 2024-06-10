ad1a7d2effd8d8fe855f250cbc168641894c8257
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2024-06-10 23:20:54 +0530

    Update Reversea_Linked_List_in_groups_of_given_size.cpp

--- Daily/Reversea_Linked_List_in_groups_of_given_size.cpp
+++ Daily/Reversea_Linked_List_in_groups_of_given_size.cpp
@@ -3,6 +3,7 @@ Given a linked list of size N. The task is to reverse every k nodes (where k is 
 of nodes is not a multiple of k then left-out nodes, in the end, should be considered as a group and must be reversed (See Example 2 for clarification).
 
 Input:
+
 LinkedList: 1->2->2->4->5->6->7->8
 K = 4
 Output: 4 2 2 1 8 7 6 5 
