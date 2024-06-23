34d95ce0aff5d5da45884ec74034b44ca0606990
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2024-06-23 20:16:49 +0530

    Update powers_of_numbers.cpp

--- Daily/powers_of_numbers.cpp
+++ Daily/powers_of_numbers.cpp
@@ -2,7 +2,6 @@
 Given a number and its reverse. Find that number raised to the power of its own reverse.
 Note: As answers can be very large, print the result modulo 109 + 7.
 
-
 Input:
 N = 2
 Output: 4
