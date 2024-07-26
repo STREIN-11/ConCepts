92eb55f6125c49935e37cce9ee503e8f59132be0
SUBHAMAY__GANGULY <71601569+STREIN-11@users.noreply.github.com>
2024-07-26 20:57:04 +0530

    Update scrambled_string.cpp

--- Daily/scrambled_string.cpp
+++ Daily/scrambled_string.cpp
@@ -7,6 +7,9 @@ Similarly, if we continue to swap the children of nodes der and er, it produces 
 Note: Scrambled string is not the same as an Anagram.
 Print "Yes" if S2 is a scrambled form of S1 otherwise print "No".
 
+
+
+
 Example 1:
 Input: S1="coder", S2="ocder"
 Output: Yes
