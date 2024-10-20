8a9c0cf1779252998d53e67fd3be93f186572024
SUBHAMAY__GANGULY <71601569+STREIN-11@users.noreply.github.com>
2024-10-20 16:03:50 +0530

    Update Make_array_beautiful.cpp

--- Daily/Make_array_beautiful.cpp
+++ Daily/Make_array_beautiful.cpp
@@ -7,6 +7,8 @@ Return the beautiful array after performing the above operation.
 Note: An empty array is also a beautiful array. There can be many adjacent integers with different signs. So remove adjacent 
 integers with different signs from left to right.
 
+
+
 Example 1:
 Input: 4 2 -2 1
 Output: 4 1
