0df22f00b7cb7434a5751bbe6397cfb6c571cbb4
SUBHAMAY__GANGULY <71601569+STREIN-11@users.noreply.github.com>
2024-11-03 23:50:58 +0530

    Update Queue_Revarsal.cpp

--- Daily/Queue_Revarsal.cpp
+++ Daily/Queue_Revarsal.cpp
@@ -2,7 +2,6 @@
 Given a Queue Q containing N elements. The task is to reverse the Queue. Your task is to complete the function rev(), that reverses the N elements of the queue.
 Input:
 6
-
 4 3 1 10 2 6
 Output: 
 6 2 10 1 3 4
